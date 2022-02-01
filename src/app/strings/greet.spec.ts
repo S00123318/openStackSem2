ac0272b36243be840c3a9a4e9f81ac2ed15261eb
Dale <S00123318@mail.itsligo.ie>
2022-02-01 11:40:25 +0000

    img added

--- src/app/strings/greet.spec.ts
+++ src/app/strings/greet.spec.ts
@@ -1,7 +1,7 @@
 import {greet} from './greet';
 
 describe ('greet', () => {
-    it('it should include the name in the message', () => {
+    it('it should include the name in the message,,, Dale Duncan - S00123318', () => {
         expect(greet('Frank')).toContain('Frank');
 
     });
